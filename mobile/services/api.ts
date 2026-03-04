c85a59085427d0b58c785eab26f116c6186361c6
Nozwelo <nicholatenozwelo@gmail.com>
2026-03-04 00:28:12 -0800

    UI testing

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/auth', // your backend base URL
+  baseURL: 'http://YOUR-IP:5000', // your backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
