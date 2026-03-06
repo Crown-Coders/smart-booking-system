31d1bbf05b094aad3229696d857f8d5e5fc20c26
Isabela <isabelachana@gmail.com>
2026-03-06 11:17:39 +0200

    user dashboard recognize logged in user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,6 +14,7 @@ router.get("/me", async (req, res) => {
       return res.status(401).json({ message: "No token provided" });
     }
 
+    
     const token = authHeader.split(" ")[1];
 
     console.log("TOKEN:", token);
--- frontend/src/Pages/users/UserDashboard.jsx
+++ frontend/src/Pages/users/UserDashboard.jsx
@@ -22,6 +22,7 @@ function UserDashboard() {
           }
         });
 
+        
         if (!response.ok) {
           throw new Error('Failed to fetch user');
         }
