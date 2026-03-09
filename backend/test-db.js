ecd7d7aab3bba4c497d2b2f9b6d2f837f5035f17
Nozwelo <nicholatenozwelo@gmail.com>
2026-03-09 14:43:30 +0200

    updating

--- backend/test-db.js
+++
@@ -1,9 +0,0 @@
-app.get("/test-db", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Database error");
-  }
-});
